a98585798b8958ba4fff8efe459ff0d70b888db8
Matvey Zhigalka <mzhigalka@gmail.com>
2024-05-31 20:45:59 +0300

    fix bug with photo

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     const body = await req.json();
-    const {email, name, password} = body
+    const { email, name, password } = body
 
     const hashedPassword = await bcrypt.hash(password, 12)
 
